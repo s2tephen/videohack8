daf47777266a26f74fbca4160c412b0e19a58f1f
catarak <ctarakajian@gmail.com>
2015-01-17 17:17:38 -0500

    get youtube live stream works

--- app/controllers/api/streams_controller.rb
+++ app/controllers/api/streams_controller.rb
@@ -1,9 +1,20 @@
+require 'open-uri'
+require 'json'
+
 class Api::StreamsController < ApplicationController
   def search
-    search_terms = params[:q]
-    videos = YoutubeClient.videos_by(:query => search_terms, :eventType => "live", :type => "video")
-    #make call to youtube API
-    #return results as JSON
-    render json: videos
+    # search_terms = params[:q]
+    # videos = YoutubeClient.videos_by(:query => search_terms, :event_type => "live", :type => "video")
+    # #make call to youtube API
+    # #return results as JSON
+
+    search_terms = URI::encode(params[:q])
+
+    base_url = "https://www.googleapis.com/youtube/v3"
+    url = "#{base_url}/search?part=snippet&q=#{search_terms}&type=video&eventType=live&key=#{ENV['youtube_api_key']}"
+    videos = open(url).read
+
+    render json: JSON.parse(videos)
   end
 end
+
