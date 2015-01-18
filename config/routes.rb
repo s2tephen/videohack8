b373c7153d9e2827a610385cc066bdfcd805882b
catarak <ctarakajian@gmail.com>
2015-01-17 19:13:04 -0500

    add root controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   namespace :api, api_options  do
     get 'streams/search' => 'streams#search'
   end
+
+  get 'home/index' => 'home#index'
+
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
